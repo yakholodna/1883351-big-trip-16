ed9f1603ca58abbbe3a6800b9dc73c17260063d6
liza <liza.kholodna@gmail.com>
2021-11-30 15:07:47 +0300

    module1-task3-try4

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import {createMenu} from './view/menu.js';
-import {createFilteredMenu} from './view/filters.js';
-import {createSortedMenu} from './view/sort.js';
+import {createFilterMenu} from './view/filters.js';
+import {createSortMenu} from './view/sort.js';
 import {createForm} from './view/create-form.js';
 import {editForm} from './view/edit-form.js';
+import {createMainTrip} from './view/destination.js';
+import {createEvent} from './view/events.js';
 
 const pageBody = document.querySelector('.page-body');
-pageBody.innerHTML = '';
+const filterContainer = document.querySelector('.trip-controls__filters');
+const navigationContainer = document.querySelector('.trip-controls__navigation');
+const tripEvents = document.querySelector('.trip-events');
+const tripDestination = document.querySelector('.trip-main');
+const EVENT_COUNT = 3;
 const positions = {
   BEFOREBEGIN: 'beforebegin',
   AFTERBEGIN: 'afterbegin',
@@ -15,8 +21,12 @@ const positions = {
 const renderElement = (container, template, location) => {
   container.insertAdjacentHTML(location, template);
 };
-renderElement(pageBody, createMenu(), positions.BEFOREEND);
-renderElement(pageBody, createFilteredMenu(), positions.BEFOREEND);
-renderElement(pageBody, createSortedMenu(), positions.BEFOREEND);
+renderElement(tripDestination, createMainTrip(), positions.AFTERBEGIN);
+renderElement(navigationContainer, createMenu(), positions.BEFOREEND);
+renderElement(filterContainer, createFilterMenu(), positions.BEFOREEND);
+renderElement(tripEvents, createSortMenu(), positions.BEFOREEND);
+renderElement(tripEvents, editForm(), positions.AFTERBEGIN);
+for (let i = 0; i < EVENT_COUNT; i++) {
+  renderElement(tripEvents, createEvent(), positions.BEFOREEND);
+}
 renderElement(pageBody, createForm(), positions.BEFOREEND);
-renderElement(pageBody, editForm(), positions.AFTERBEGIN);
